0083c3c21e3bd85aa023c70bd6ef24c016ea6b72
Phil Kunz <gitlab@philkunz.com>
2019-08-22 15:41:49 +0200

    fix(core): update

--- ts/index.ts
+++ ts/index.ts
@@ -1,8 +1,5 @@
 import * as plugins from './typedrequest-interfaces.plugins';
 
-export let standardExport = 'Hi there! :) This is an exported string';
-
-
 export interface ITypedRequest {
   method: string;
   request: object;
