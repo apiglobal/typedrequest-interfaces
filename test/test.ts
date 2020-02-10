62f3b028128d10ddd96c9273888bb1898c612402
Phil Kunz <gitlab@philkunz.com>
2020-02-10 21:06:51 +0000

    fix(core): update

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,12 @@
 import { expect, tap } from '@pushrocks/tapbundle';
 import * as typedrequestInterfaces from '../ts/index';
 
+interface IRequestSample extends typedrequestInterfaces.implementsTR<typedrequestInterfaces.ITypedRequest, IRequestSample> {
+  method: 'hey';
+  request: {};
+  response: {};
+}
+
 tap.test('first test', async () => {
   typedrequestInterfaces;
 });
--- ts/index.ts
+++ ts/index.ts
@@ -2,7 +2,14 @@ export interface ITypedRequest {
   method: string;
   request: object;
   response: object;
+  error?: { text: string, data: any };
+  retry?: {
+    waitForMs: number;
+    reason: string;
+  };
 }
+
+export type implementsTR<T, U extends T> = {};
 
 export interface IBroadCastEvent<T> {
   name: string;
